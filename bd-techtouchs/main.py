725afcadd482b88086cea41e729ef0b99a9cdc06
pedroviniciusalvesdiniz <alvesdinizpedrovinicius@gmail.com>
2026-01-18 11:30:40 -0300

    opção de cadastrar um novo usuário ao finalizar o cadastro atual

--- bd-techtouchs/main.py
+++ bd-techtouchs/main.py
@@ -4,15 +4,21 @@ from redirect_wpp import redirect_wpp
 
 
 def main():
-    url = input( "Digite a url do perfil: ")
-    dados_lead = take_info_lead(url)
-    telefone = dados_lead[-1]
-    primeiro_nome = dados_lead[0].split()[0]
-    print(dados_lead)
+    while True:
+        url = input( "Digite a url do perfil: ")
+        dados_lead = take_info_lead(url)
+        telefone = dados_lead[-1]
+        primeiro_nome = dados_lead[0].split()[0]
+        print(dados_lead)
 
-    if dados_lead != None:
-        inserir_dados(dados_lead)
-        redirect_wpp(primeiro_nome, telefone)
+        if dados_lead != None:
+            inserir_dados(dados_lead)
+            # redirect_wpp(primeiro_nome, telefone)
+
+            continuar = input("Deseja continuar? (1 para sim, outra tecla para não): ")
+            if continuar != '1':
+                break
+    
     
 
 if __name__ == "__main__":

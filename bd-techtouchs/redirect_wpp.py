54da13ee8ef885312ca63ba0a62e96f48f4b9e2c
pedroviniciusalvesdiniz <alvesdinizpedrovinicius@gmail.com>
2026-02-19 08:24:48 -0300

    update

--- bd-techtouchs/redirect_wpp.py
+++ bd-techtouchs/redirect_wpp.py
@@ -2,5 +2,5 @@ import webbrowser
 
 def redirect_wpp(nome, telefone):
     # print(telefone)
-    mensagem = f"Oi, {nome}. Sou da Processo Ágil. Vi seu cadastro. Consigo te ajudar em algo?"
+    mensagem = f"Oi, {nome}. Sou da Processo Ágil. Vi seu cadastro e queria entender: O que te levou a buscar nossa plataforma?"
     webbrowser.open(f"https://api.whatsapp.com/send?phone={telefone}&text={mensagem.replace(' ', '%20')}")

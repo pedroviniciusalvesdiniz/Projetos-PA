d339d0ac8aa6114438b1cf0cc921f0c50e0ac6d5
Pedro Vinicius Alves Diniz <pedrinhovini318@icloud.com>
2026-02-24 21:46:50 -0300

    update

--- jornada-vendedor/jornada/src/app/layout.js
+++ jornada-vendedor/jornada/src/app/layout.js
@@ -18,9 +18,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning
       >
         {children}
       </body>
